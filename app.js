d53128a706f35fd10c0752ad636cb83dd0ccc556
Srajan jain <110524518+jainsrajan@users.noreply.github.com>
2024-05-29 22:31:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:"https://musical-biscuit-f07d19.netlify.app",
     credentials: true
 }))
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*" );
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type" , "X-Auth-Token" , "Origin" , "Authorization");
-    next();
-  })
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*" );
+//     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,DELETE");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type" , "X-Auth-Token" , "Origin" , "Authorization");
+//     next();
+//   })
 
 app.use(morgan('dev'))
 
