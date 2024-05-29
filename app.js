d0959b6e8e14d78b4cb1fe469f48407c34c8a910
Srajan jain <110524518+jainsrajan@users.noreply.github.com>
2024-05-29 21:33:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(cors({
     credentials: true
 }))
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin",process.env.FRONTEND_URL );
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
+
 app.use(morgan('dev'))
 
 app.use(cookieParser())
