7cb3da2c40f6b223b9acd880f9198e18bfd5f587
Srajan jain <110524518+jainsrajan@users.noreply.github.com>
2024-05-30 22:07:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors({
     origin:'https://bucolic-mooncake-057a17.netlify.app',
      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
+    credentials:true
 }))
 
 // app.use((req, res, next) => {
